e80dd42e8c4fa8229c18d2b5f7b1e7258fed84d4
hollycosslett <154456339+hollycosslett@users.noreply.github.com>
2024-05-03 11:06:27 +0100

    added link

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,23 @@
 import React from "react";
 
 export default function Footer() {
-    return (<footer>
-        This project was coded by
-        <a href="#" target="_blank"
-          > Holly Cosslett</a
-        >, is
-        <a
-          href="#"
-          target="_blank"
-        > open-sourced on GitHub</a> and
-        <a
-          href="#"
-          target="_blank"
-          > hosted on Netlify</a>.
-      </footer>
-
-    )
+  return (
+    <footer>
+      This project was coded by
+      <a href="https://github.com/hollycosslett" target="_blank">
+        {" "}
+        Holly Cosslett
+      </a>
+      , is
+      <a href="#" target="_blank">
+        {" "}
+        open-sourced on GitHub
+      </a>{" "}
+      and
+      <a href="#" target="_blank">
+        {" "}
+        hosted on Netlify.
+      </a>
+    </footer>
+  );
 }
